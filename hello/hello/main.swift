95ffdf41a3eb3439b0806de8e28a2767a75e1445
Kevin Marciniak <kevin.marciniak@gmail.com>
2017-04-27 19:58:11 -0500

    Hello World!
    Very simple Swift program with Xcode project file.

---
+++ hello/hello/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  hello
+//
+//  Created by Kevin Marciniak on 4/27/17.
+//  Copyright © 2017 Kevin Marciniak. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+print("Olá. Mundo!")
