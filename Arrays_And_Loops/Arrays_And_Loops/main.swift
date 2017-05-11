58686de58f57e13f90a3b9b7a833e6e0c369da8b
Kevin Marciniak <kevin.marciniak@gmail.com>
2017-05-11 12:15:27 -0500

    Lesson 14 Solved
    Solves a couple of the exercises from Intro to App Development with
    Swift

---
+++ Arrays_And_Loops/Arrays_And_Loops/main.swift
@@ -0,0 +1,54 @@
+//
+//  main.swift
+//  Arrays_And_Loops
+//
+//  Created by Kevin Marciniak on 5/11/17.
+//  Copyright © 2017 Kevin Marciniak. All rights reserved.
+//
+
+import Foundation
+
+let shouldMascotChangeVotes: [Bool] = [false, false, false, true, false, true, true, true, false, true, true, true, true, false, true, true, false, true, true, true, false, true, true, true, true, true, true, true, false, true, false, true, false, true, true, false, false, true, true, false, false, true, true, true, false, true, false, true, true, false, true, true, false, true, false, false, true, false, true, true, false, false, true, false, true, true, true, false, true, true, false, false, true, false, true, true, false, false, false, true, false, true, true, false, true, true, true, true, true, true, true, false, true, false, true, false, true, true, true, true, true, true, true, false, true, true, false, true, true, true, true, true, true, true, false, true, true, false, true, true, false, true, true, true, true, true, false, false, false, false, true, true, true, false, true, true, false, false, true, false, false, true, true, true, true, false, true, true, true, true, false, true, true, false, true, false, false, true, true, false, true, false, false, false, true, false, false, false, true, false, true, true, false, true, true, false, true, true, true, false, false, false, true, false, true, false, true, true, true, true, false, true, false, false, true, true, true, true, true, false]
+
+let shouldInstallCoffeeVendingMachineVotes: [Bool] = [true, true, false, false, false, true, true, false, true, true, true, true, false, true, false, false, true, false, true, false, true, true, false, false, false, false, false, true, true, true, false, false, true, true, false, true, true, true, true, false, true, false, true, true, false, false, false, false, false, false, true, false, true, true, false, true, true, true, true, false, false, true, true, false, false, false, false, true, true, false, false, true, true, true, true, false, false, true, true, false, true, false, true, false, true, true, true, false, true, true, true, false, false, false, false, false, false, false, false, false, false, false, true, false, true, false, false, true, true, false, true, false, true, true, true, false, false, false, false, false, false, true, true, false, false, true, true, true, true, true, true, false, false, false, true, true, true, true, false, false, false, true, true, false, true, true, true, false, false, true, false, true, false, true, false, false, true, false, true, true, true, true, true, true, true, false, true, false, true, true, false, false, true, false, false, true, false, false, false, true, false, true, true, true, false, false, false, false, false, false, true, false, true, false, true, true, false, false, false, true]
+
+let shouldHaveMorePollOptionsVotes: [Bool] = [false, false, true, true, false, true, false, false, false, false, false, false, true, false, true, true, false, true, true, false, false, true, true, false, false, false, false, false, false, false, true, false, false, false, false, true, false, false, false, false, false, false, true, true, false, true, true, false, true, false, true, true, false, false, false, false, true, false, true, true, false, false, false, false, true, true, true, true, false, true, false, false, true, true, false, false, false, false, false, false, true, true, false, false, false, false, false, true, true, false, false, false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, false, false, true, false, true, false, false, false, true, false, true, true, true, true, true, true, true, false, false, false, false, true, false, false, false, false, false, true, false, false, true, false, false, true, false, false, true, false, false, true, false, false, true, false, false, false, false, false, true, false, false, false, false, false, false, true, true, true, false, true, false, false, false, false, false, false, false, false, true, true, true, true, false, true, true, false, false, true, false, true, true, false, false, true, true, false, true, false, false, false, true, true, false, false]
+
+
+func printResults(forIssue: String, withVotes: [Bool]) {
+    var yes = 0
+    var no = 0
+    var finalStatement = forIssue
+    for votes in withVotes {
+        if votes {
+            yes += 1
+        } else {
+            no += 1
+        }
+    }
+    finalStatement += " \(yes) yes, \(no) no"
+    print(finalStatement)
+}
+
+printResults(forIssue: "Should we change the mascot?", withVotes:shouldMascotChangeVotes)
+printResults(forIssue: "Should we install a coffee vending machine?", withVotes:shouldInstallCoffeeVendingMachineVotes)
+printResults(forIssue: "Should we have more poll options?", withVotes:shouldHaveMorePollOptionsVotes)
+print()
+
+let milesBiked = [4, 8, 2, 10, 0, 0, 3, 6, 7, 8, 11, 0, 0, 20, 2, 3, 0, 0, 0, 8]
+
+func onTarget(_ actual: Int) -> String {
+    let target = 8
+    
+    if actual >= (target - 1) && actual <= (target + 1) {
+        return "On target!"
+    } else if actual < (target - 1) {
+        return "Below target!"
+    } else {
+        return "Above target, don't try too hard!"
+    }
+}
+
+for dailyMilesBiked in milesBiked {
+    print(onTarget(dailyMilesBiked))
+}
